d2c27b75da5b160da2b080e4dd2a195e9cdcc7ad
animesh-hub01 <ani09space22@gmail.com>
2026-09-22 23:11:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (1.78%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        if (num2>=0)
-            for(int i=0; i<num2; i++){
-                num1+=1;
-            }
-        else
-            for(int i=0; i<abs(num2); i++){
-                num1-=1;
-            }
-        
+        while(num2){
+            int carry = (num1 & num2) << 1;
+            num1 = (num1 ^ num2);
+            num2 = carry;
+        }
         return num1;
     }
 };
