e2fb3b2171313950f4daf095c0ebed65fc1b1b77
animesh-hub01 <ani09space22@gmail.com>
2026-09-23 19:33:45 +0530

    Time: 11 ms (91.36%), Space: 19.7 MB (35.27%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_counts = Counter(magazine)
+        
+        for char in ransomNote:
+            if magazine_counts[char] <= 0:
+                return False
+            magazine_counts[char] -= 1
+            
+        return True
